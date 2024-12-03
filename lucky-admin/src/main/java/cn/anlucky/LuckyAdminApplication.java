94e69a6d9ff39690a05ac759032aade843d241b9
DuYiFan <duyifan@xiyiqq.com>
2024-12-03 16:05:33 +0800

    创建项目

---
+++ lucky-admin/src/main/java/cn/anlucky/LuckyAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.anlucky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyAdminApplication.class, args);
+    }
+
+}
---
+++ lucky-admin/src/test/java/cn/anlucky/LuckyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.anlucky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
